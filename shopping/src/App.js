aed4a4ba1a95dc80276f118d5d54a5e9b72977cf
junhyungkwon <wnsgud8845@gmail.com>
2022-12-23 00:20:35 +0900

    Bootstrap 초기설정, test

--- shopping/src/App.js
+++ shopping/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+import { Button } from 'react-bootstrap';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Button variant="primary">Primary</Button>{' '}
     </div>
   );
 }
