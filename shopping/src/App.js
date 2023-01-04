a328935597b31bc666dd6e6fb7220e66c1cd50ff
junhyungkwon <wnsgud8845@gmail.com>
2023-01-04 23:00:46 +0900

    import export, 컴포넌트화 하여 코드 길이 줄이기

--- shopping/src/App.js
+++ shopping/src/App.js
@@ -2,9 +2,15 @@ import { Button, Navbar,Container, Nav } from 'react-bootstrap';
 import './App.css';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import { useState } from 'react';
+import data from './data.js';
+import Main from './component';
 
 
 function App() {
+
+  let [shop, Setshop] = useState(data);
+  
   return (
     <div className="App">
       
@@ -15,30 +21,30 @@ function App() {
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#features">Cart</Nav.Link>
           </Nav>
-
-          
         </Container>
       </Navbar>
 
       <div className='main-bg'></div>
-
+    
       <div className="container">
    <div className="row">
-    <div className="col-md-4">
-      <img src='https://user-images.githubusercontent.com/78072931/210526606-d26a9e11-c226-41c6-9b53-ee1f93d2a184.png'  width="60%"/>
-      <h4> 상품명 </h4>
-      <p> 상품 설명 </p>
-    </div>
-    <div className="col-md-4">
+    
+    <Main/>
+    <Main/>
+    <Main/>
+    {/* <div className="col-md-4">
       <img src='https://user-images.githubusercontent.com/78072931/210526775-45912dc2-a771-4418-ba7d-00a712e503f8.png' width='59%'/>
-      <h4> 상품명 </h4>
-      <p> 상품 설명 </p>
+      <h4> {shop[1].title} </h4>
+      <p> {shop[1].content} </p>
+      <p> {shop[1].price} </p>
     </div>
     <div className="col-md-4">
       <img src='https://user-images.githubusercontent.com/78072931/210526097-feeceb6f-b698-4b09-96f2-7ceca1657f1b.png' width='58%'/>
-      <h4> 상품명 </h4>
-      <p> 상품 설명 </p>
-    </div>
+      <h4> {shop[2].title} </h4>
+      <p> {shop[2].content} </p>
+      <p> {shop[2].price} </p>
+    </div> */}
+    
 
   </div>
 </div> 
