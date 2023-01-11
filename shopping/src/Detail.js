4b850724020270370bd1738ce4574e5c42f12baf
junhyungkwon <wnsgud8845@gmail.com>
2023-01-11 17:30:28 +0900

    Lifecycle, useEffect React Hook를 사용하여 구현

--- shopping/src/Detail.js
+++ shopping/src/Detail.js
@@ -1,16 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
  
 function Detail(props){
-  let {id} = useParams();
-  let 찾은상품 = props.shoes.find(x=> x.id == id);{
-    
 
-  }
+  useEffect (()=>{
+    
+  })
+  
+  setTimeout(() => {
+    
+  }, 2000);
+
+  let [count, setCount] = useState(0);
+  let [alert, setAlert] = useState(true)
+
+
+  let {id} = useParams();
+  let 찾은상품 = props.shoes.find(x=> x.id == id);
+    
 
     return( 
     <div className="container">
+      <div className="alert alert-warning">
+        2chdlso 
+      </div>
+      {count}
+      <button onClick={()=> {
+        setCount(count+1) }}> 버튼입니다 </button>
+
     <div className="row">
       <div className="col-md-6">
         <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
